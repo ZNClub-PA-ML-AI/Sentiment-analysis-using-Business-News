655ce24f89d12ba3ccdf216d99796807ed2f9943
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-01-09 13:08:36 +0530

    keyword filter applied

--- keyword_extraction.py
+++ keyword_extraction.py
@@ -6,18 +6,65 @@ Created on Mon Jan  9 10:42:03 2017
 """
 
 import re
+import pandas as pd
 
 #print(re.search("tara","tcs "))
 
-news="\u200bICICI Pru makes tepid debut, stock plunges over 5% from issue price"
-news = news.lower()
-print(news)
-tokens = ["tcs","tata consultancy services"] 
+    ## read text and pattern files
 
 
-for i in tokens:
-    #print(i,news)
-    if re.search(i,news):
-        print("found")
-    else:
-        print("not found")
+    ## text file
+file_name = ['economictimes_data.csv','livemint_data.csv']
+#
+#df = pd.read_csv(file_name[0],encoding='iso-8859-1')
+#print(df.head())
+
+df = pd.read_csv(file_name[1],encoding='iso-8859-1')
+print(df.head())
+
+#print(df['title'])
+news_list=df['title'].tolist()
+#print(len(news_list))
+
+
+    ## pattern file
+df = pd.read_excel('company_keyword.xlsx', sheetname='Sheet1')
+#print(df.head())
+#print(df['company'].count())
+#
+#describe=df.describe()
+#print(describe.company)
+
+#
+#count=0
+#for index, row in df.iterrows():
+#        #print(row.company,row.keyword)
+#        k = row.company
+#        v = row.keyword
+#        v_list = v.split(',')
+##        print(v_list)
+#        for i in v_list:
+#            print(i)
+#            count+=1
+#
+#print(count)
+count=0
+for news in news_list:
+    news = news.lower()
+    #print(news)
+    
+    for index, row in df.iterrows():
+        #print(row.company,row.keyword)
+        k = row.company
+        v = row.keyword
+        v_list = v.split(',')
+        #print(k)
+        for i in v_list:
+            i=i.lower()
+            if re.search(i,news):
+                print("-------"+news+k)
+                count+=1
+
+print(count)           
+print(len(news_list))    
+    
