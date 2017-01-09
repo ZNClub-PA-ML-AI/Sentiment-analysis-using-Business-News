ef1df305c16e7d0ae6c7dc4d95d434e28d1aa4c5
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-01-09 11:08:07 +0530

    read datasets as csv files

--- io.py
+++ io.py
@@ -2,9 +2,20 @@
 """
 Created on Mon Jan  9 09:25:26 2017
 
-@author: universe
+@author: ZNevzz
 """
 
 import pandas as pd
+
 df = pd.read_excel('company_keyword.xlsx', sheetname='Sheet1')
+print(df.head())
+
+file_name = 'economictimes_data.csv'
+
+df = pd.read_csv(file_name,encoding='iso-8859-1')
+print(df.head())
+
+file_name = 'livemint_data.csv'
+
+df = pd.read_csv(file_name,encoding='iso-8859-1')
 print(df.head())
