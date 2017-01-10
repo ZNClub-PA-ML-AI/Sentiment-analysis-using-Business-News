83bba2e31a544e4dab62a9d453fb8dca0888f34b
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-01-10 15:43:56 +0530

    ngrams added

---
+++ ngram.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 15:12:06 2017
+
+@author: ZNevzz
+"""
+import nltk
+import codecs
+from nltk import word_tokenize
+from nltk.util import ngrams
+from collections import Counter
+
+def read():
+    with codecs.open('sample_news.txt','r',encoding='utf8') as f:
+        text = f.read()
+        text = text.lower()
+        #print(text)
+        return text
+        
+        
+    
+#bigrams = ngrams(token,2)
+#trigrams = ngrams(token,3)
+#fourgrams = ngrams(token,4)
+#fivegrams = ngrams(token,5)
+
+    ## read file
+text = read()
+
+tokens = nltk.word_tokenize(text)
+#print(tokens)
+
+unigram = tokens
+
+#print(unigram)
+bigrams = ngrams(tokens,2)
+#print(Counter(bigrams))
+trigrams = ngrams(tokens,3)
+#print(Counter(trigrams))
+
+
+
