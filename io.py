3c88ec224d33bad7aaacd2c836856a0afc6bd74b
ZNevzz <dsouza.nevil45@gmail.com>
2017-01-23 18:29:56 +0530

    io test success

--- io.py
+++ io.py
@@ -6,20 +6,24 @@ Created on Mon Jan  9 09:25:26 2017
 """
 
 import pandas as pd
+import codecs
 
 df = pd.read_excel('company_keyword.xlsx', sheetname='Sheet1')
-print(df.head())
+#print(df.head())
+print(df.shape)
 
 file_name = 'economictimes_data.csv'
 
 df = pd.read_csv(file_name,encoding='iso-8859-1')
-print(df.head())
+#print(df.head())
+print(df.shape)
 
 file_name = 'livemint_data.csv'
 
 df = pd.read_csv(file_name,encoding='iso-8859-1')
-print(df.head())
+#print(df.head())
+print(df.shape)
 
   ## read file with encodings
-with codecs.open(filename,'r',encoding='utf8') as f:
-    text = f.read()
+#with codecs.open(file_name,'r',encoding='utf8') as f:
+#    text = f.read()
