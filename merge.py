bd93237191b7a54b19a8c25712a1be253eb7c99c
ZNevzz <dsouza.nevil45@gmail.com>
2017-03-13 11:49:04 +0530

    uniquely identified 1467 articles

--- merge.py
+++ merge.py
@@ -22,8 +22,19 @@ filenames=['data_o1.csv','data_o2.csv']
 df1=pd.read_csv(filenames[0])
 df2=pd.read_csv(filenames[1])
 #print(df2.head(1))
+#
+#result = pd.concat([df1, df2], axis=1, join_axes=[df1.index])
+#print(result.describe())
+#result.to_csv('data_joined_1.csv', sep=',', encoding='utf-8')
 
-result = pd.concat([df1, df2], axis=1, join_axes=[df1.index])
-print(result.describe())
-result.to_csv('data_joined_1.csv', sep=',', encoding='utf-8')
+# create sets of ids
+
+id_df1=set(df1['id'].tolist())
+print(len(id_df1))
+
+id_df2=set(df2['id'].tolist())
+print(len(id_df2))
+
+ids=id_df1.intersection(id_df2)
+print(len(ids))
 
