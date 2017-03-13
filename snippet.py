5580562a454deaf01d2928ddbf19c4962580cf4b
ZNevzz <dsouza.nevil45@gmail.com>
2017-03-13 11:17:50 +0530

    code added to snippet

--- snippet.py
+++ snippet.py
@@ -5,9 +5,10 @@ Created on Fri Feb  3 08:21:39 2017
 @author: ZNevzz
 """
 import re
-from nltk.corpus import sentiwordnet as swn
-"""
+import pandas as pd
+
     ## REGEX FILTER ALGO START
+"""    
 text = '''
 <p class="xh-highlight">Singed by the choice made by its predecessor, the current committee set up last October to look for a successor to run the Tata Group, has made the eminently safer choice of a chairman who has no other interests in the group apart from the mandated task of running it. In selecting group insider Natarajan Chandrasekaran, chief executive of Tata Consultancy Services Ltd since October 2009 and a TCS lifer, having joined the company straight after completing his Masters in Computer Applications from Regional Engineering College, Trichy, the committee has clearly decided to play safe this time around.</p><p class="xh-highlight">
 				   
@@ -57,15 +58,16 @@ abc…	Letters
     ## REGEX FILTER ALGO END
 
     ## ID EXTRACTION ALGO START
-import pandas as pd
+
 file_name = 'livemint_data.csv'
 
 df = pd.read_csv(file_name,encoding='iso-8859-1')
 #print(df.head())
 print(df.shape) 
-   
+
     ## ID EXTRACTION ALGO END
-    
+
+    ## 
     
     
     
