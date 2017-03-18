b335d8ea1eafaea38a3a9b3bb1da0de1b05fcd71
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-03-18 15:42:18 +0530

    Test

--- preprocessor.py
+++ preprocessor.py
@@ -1,4 +1,3 @@
-
 """
 Created on Mon Mar 18 11:27:40 2017
 @author: ZNevzz
@@ -9,6 +8,6 @@ import pandas as pd
 #read file
 filenames=['data_o2.csv']
 
+html='<p xmlns:fn=""http://www.w3.org/2005/xpath-functions"" id=""U10478744187zRC"" style="" letter-spacing: -7; ;"">The December quarter result of auto component'
 
-
-html='<p xmlns:fn=""http://www.w3.org/2005/xpath-functions"" id=""U10478744187zRC"" style="" letter-spacing: -7; ;"">The December quarter result of auto component maker Motherson Sumi Systems Ltd (MSSL) was a pleasant surprise for investors. A 23% rise in consolidated operating profit from a year earlier was driven partly by higher sales, and by lower input costs. Just six months ago, the Street punished this stock on fears of a massive sales pullback after its major customer Volkswagen AG suffered a setback on account of the emission tests scandal in the US.<p id=""U10478744187s9B"" style="" letter-spacing: -7; ;"">'
+print(BeautifulSoup.(html).get_text())
