7c6ff222e63592c91f36e754d0c122193a6ab669
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-03-18 15:22:06 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -11,5 +11,4 @@ filenames=['data_o2.csv']
 
 
 
-html=''
-
+html='<p xmlns:fn=""http://www.w3.org/2005/xpath-functions"" id=""U10478744187zRC"" style="" letter-spacing: -7; ;"">The December quarter result of auto component maker Motherson Sumi Systems Ltd (MSSL) was a pleasant surprise for investors. A 23% rise in consolidated operating profit from a year earlier was driven partly by higher sales, and by lower input costs. Just six months ago, the Street punished this stock on fears of a massive sales pullback after its major customer Volkswagen AG suffered a setback on account of the emission tests scandal in the US.<p id=""U10478744187s9B"" style="" letter-spacing: -7; ;"">'
