92729734a01db80b5c70562ebe0434d254c826ee
root <root@beproject.c.luminous-girder-160413.internal>
2017-03-18 10:23:40 +0000

    testing 2

--- preprocessor.py
+++ preprocessor.py
@@ -10,4 +10,8 @@ filenames=['data_o2.csv']
 
 html='<p xmlns:fn=""http://www.w3.org/2005/xpath-functions"" id=""U10478744187zRC"" style="" letter-spacing: -7; ;"">The December quarter result of auto component'
 
-print(BeautifulSoup.(html).get_text())
+#print(BeautifulSoup.(html).get_text())
+t=BeautifulSoup(html)
+tt=t.get_text()
+print(tt)
+
