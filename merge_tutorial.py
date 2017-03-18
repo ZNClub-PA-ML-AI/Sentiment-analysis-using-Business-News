9ee8755bc690d95c24db85aa67673738818905c4
root <root@beproject.c.luminous-girder-160413.internal>
2017-03-18 16:10:26 +0000

    sample merge success

---
+++ merge_tutorial.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df1 = pd.DataFrame({'A': ['A0', 'A1'],'B':['B0','B1']},index=[0,1])
+df2 = pd.DataFrame({'C':['C0','C1']},index=[0,1])
+
+df3= pd.concat([df1,df2],axis=1)
+print(df3.head())
+
