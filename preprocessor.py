408a085410abaf25aaa3536b5838235d7345d957
root <root@beproject.c.luminous-girder-160413.internal>
2017-03-18 10:25:31 +0000

    snippet working

--- preprocessor.py
+++ preprocessor.py
@@ -11,7 +11,7 @@ filenames=['data_o2.csv']
 html='<p xmlns:fn=""http://www.w3.org/2005/xpath-functions"" id=""U10478744187zRC"" style="" letter-spacing: -7; ;"">The December quarter result of auto component'
 
 #print(BeautifulSoup.(html).get_text())
-t=BeautifulSoup(html)
+t=BeautifulSoup(html,"lxml")
 tt=t.get_text()
 print(tt)
 
