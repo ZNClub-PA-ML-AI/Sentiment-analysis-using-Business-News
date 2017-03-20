9be606363e4f080ad9ca2cefea72b4f8fe48acd5
root <root@beproject.c.luminous-girder-160413.internal>
2017-03-20 08:28:33 +0000

    sentiment testing successfull

--- sentiment.py
+++ sentiment.py
@@ -25,7 +25,32 @@ def sentiment_cal(title,intro,body):
 sia = SentimentIntensityAnalyzer()
 
 t_list = df.title.tolist()[:4]
+ts=[]
 for t in t_list:
 	ps = sia.polarity_scores(t)
-	print(ps['compound'])
+	print(ps['compound'],ps['pos'],ps['neg'],ps['neu'])
+	ts.append(ps['compound'])	
+
+i_list = df.intro.tolist()[:4]
+iis=[]
+for i in i_list:
+	ps = sia.polarity_scores(i)
+	print(ps['compound'],ps['pos'],ps['neg'],ps['neu'])
+	iis.append(ps['compound'])
+
+b_list = df.body.tolist()[:4]
+bs=[]
+for b in b_list:
+	ps = sia.polarity_scores(b)
+	print(ps['compound'],ps['pos'],ps['neg'],ps['neu'])
+	bs.append(ps['compound'])
+
+score=[0 for i in range(4)]
+
+print(ts,iis,bs)
+
+for i in range(0,4):
+	print(i,ts[i],iis[i],bs[i])
+	score[i]=ts[i]+(iis[i]*float(0.5))+(bs[i]*float(0.25))
+print(score)
 
