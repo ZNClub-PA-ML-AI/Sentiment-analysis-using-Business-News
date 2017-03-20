02c9577ecde33a6e9cd934886d3922c5b3a54e3c
root <root@beproject.c.luminous-girder-160413.internal>
2017-03-20 05:05:48 +0000

    testing time formating

--- normalizer.py
+++ normalizer.py
@@ -25,7 +25,7 @@ print(splitter)
 
 month={'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06','Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
 
-date = splitter[3][0:-2]+'-'+month[splitter[1]]+'-'+splitter[2]
+date = splitter[3][0:-1]+'-'+month[splitter[1]]+'-'+splitter[2]
 print(date)
 
 
