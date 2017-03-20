addf26cdfcbe3aaf250aa62dddbc8929f806ab80
root <root@beproject.c.luminous-girder-160413.internal>
2017-03-20 04:12:53 +0000

    normalizer.py added

--- normalizer.py
+++ normalizer.py
@@ -10,12 +10,12 @@ import pandas as pd
 
 filenames = ['data_joined_2.csv']
 df = pd.read_csv(filenames[0])
-print(df.head(2))
+#print(df.head(2))
+print(df.columns)
 
 # drop last id column
 
-
-df.drop(df.columns[5],axis=1,inplace=True)
+df.drop(df.columns[6],axis=1,inplace=True)
 print(df.columns)
 
 
