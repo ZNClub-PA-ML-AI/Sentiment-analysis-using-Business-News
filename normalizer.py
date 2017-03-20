8cae698e13989c5d0e3f5e5d739827b2d812f513
root <root@beproject.c.luminous-girder-160413.internal>
2017-03-20 04:05:00 +0000

    testing drop function

--- normalizer.py
+++ normalizer.py
@@ -6,7 +6,16 @@ Created on Mon Mar 19 9:21:10 2017
 
 import pandas as pd
 
+# read file
+
 filenames = ['data_joined_2.csv']
 df = pd.read_csv(filenames[0])
 print(df.head(2))
+
+# drop last id column
+
+
+df.drop(df.columns[5],axis=1,inplace=True)
+print(df.columns)
+
 
