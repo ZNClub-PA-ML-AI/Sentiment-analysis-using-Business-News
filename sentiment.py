2927962c6100368b18d4d558e7d8f9830a28c7e5
root <root@beproject.c.luminous-girder-160413.internal>
2017-03-20 07:44:23 +0000

    title sentiment works

--- sentiment.py
+++ sentiment.py
@@ -2,12 +2,15 @@
 Created on Mon Mar 20 11:41:40 2017
 
 @author: ZNevzz
-""
+"""
+
 import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk import tokenize
 
 # read file
+filenames=['normalized.csv']
+df = pd.read_csv(filenames[0])
 
 def sentiment_cal(title,intro,body):
 	
@@ -18,5 +21,17 @@ def sentiment_cal(title,intro,body):
 	return
 
 
+# test vader with title
 
+t_list = df.title.tolist()[:4]
+print(len(t_list))
+sia = SentimentIntensityAnalyzer()
+
+for t in t_list:
+
+	ps = sia.polarity_scores(t)
+	print(ps['compound'])
+
+
+print("ends")
 
