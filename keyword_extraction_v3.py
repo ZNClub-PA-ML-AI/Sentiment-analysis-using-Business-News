f80efc9ebd87a20f9c0a40e955ca3b1bf20a32c4
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-03-22 20:33:51 +0530

    test read

--- keyword_extraction_v3.py
+++ keyword_extraction_v3.py
@@ -1,1 +1,17 @@
-import pandas
+"""
+Created on Mon Mar 20 11:41:40 2017
+
+@author: ZNevzz
+"""
+
+import pandas as pd
+from collections import defaultdict
+
+
+# read file
+filenames=['labeled.csv','company_keyword.xlsx']
+df = pd.read_csv(filenames[0])
+
+print(df.describ(),df.head())
+
+
