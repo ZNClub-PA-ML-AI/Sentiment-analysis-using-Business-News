39418004b2eb5aec46aa8f9bc4a0caadf7a64266
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-03-23 07:35:19 +0530

    moved k.lower within its scope

--- keyword_extraction_v3.py
+++ keyword_extraction_v3.py
@@ -12,19 +12,20 @@ import re
 filenames=['labeled.csv','company_keyword.xlsx']
 df = pd.read_csv(filenames[0])
 
-print(df.describe(),df.head())
+#print(df.describe(),df.head())
 
 helper = defaultdict(list)
 keywords='Reliance Industries,Mukesh Ambani,Anil Ambani,Reliance Commercial Corporation,RELIANCE,RIL,Jio'
 helper['REL']=keywords.split(',')
 
-print(helper)
+#print(helper)
 c=0
 for i,r in df.iterrows():
 	t=str(r.title).lower()
 	i=str(r.intro).lower()
-	pattern=k.lower()
+	
 	for k in helper['REL']:
+		pattern=k.lower()
 		if re.search(pattern,i) or re.search(pattern,t):
 			c=c+1
 			break
