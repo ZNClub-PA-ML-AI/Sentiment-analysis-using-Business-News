c85189e6db26e143f3bad7d76a9ca3f7c17c63bb
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-03-23 21:06:15 +0530

    unique dates

--- pre_prediction.py
+++ pre_prediction.py
@@ -1,3 +1,10 @@
-impor
+import pandas as pd
 
+df = pd.read_csv('REL.csv')
+dates=set(df.date.tolist())
+print(len(dates))
+
+
+#df = df.set_index(['date'])
+#df.to_csv('REL.csv',sep=',',encoding='utf-8')
 
