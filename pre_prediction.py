cae2bba88d42a8f2cf958a246307783fbe074959
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-03-24 07:24:12 +0530

    Counter added

--- pre_prediction.py
+++ pre_prediction.py
@@ -1,10 +1,23 @@
 import pandas as pd
+import collections
 
 df = pd.read_csv('REL.csv')
-dates=set(df.date.tolist())
-print(len(dates))
+date_list = df.date.tolist
+dates=set(date_list)
+#print(len(dates))
 
+result=pd.DataFrame()
+cnt = collections.Counter(date_list)
+print(len(cnt))
+
+"""
+for i,r in df.iterrows():
+	date=str(r.date)
+	if prev=-1 or prev!=date:
+		prev=
+"""
 
 #df = df.set_index(['date'])
 #df.to_csv('REL.csv',sep=',',encoding='utf-8')
 
+    
