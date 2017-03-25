19544e337355554fbcab911d7af29d2ac85266fe
root <root@beproject.c.luminous-girder-160413.internal>
2017-03-25 14:35:25 +0000

    ascending order of nse reliance csv for highcharts stock candlestick

--- csv_json.py
+++ csv_json.py
@@ -9,9 +9,10 @@ import pandas as pd
 filename=str(sys.argv[1])
 df=pd.read_csv(filename)
 #df = df.set_index(['date'])
+df = df.sort_values(['Date'],ascending=[1])
 print(df.head(1))
 #print(filename[:-3])
-df.to_json(filename[:-3]+'json')
+#df.to_json(filename[:-3]+'json')
 
 
 
