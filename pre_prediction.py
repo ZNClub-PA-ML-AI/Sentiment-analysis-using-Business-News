029ad075f9407e9f129744a5726bcbb019be1adb
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-03-25 11:24:28 +0530

    time logic

--- pre_prediction.py
+++ pre_prediction.py
@@ -1,3 +1,9 @@
+"""
+Created on Thu Mar 23 20:58:21
+
+@author: ZNevzz
+"""
+
 import pandas as pd
 import collections
 
@@ -17,12 +23,25 @@ for k,v in od.items():
 	print(k,v)
 	score[k]=0
 
-#print(od['2015-11-04'])
-
 
 for i,r in df.iterrows():
 	date=str(r.date)
-	score[date]+=float(r.score)
+	time=datetime().datetime().strptime(str(r.time),'%H:%M')
+	time_open=datetime().datetime().strptime('09:00','%H:%M')
+	time_close=datetime().datetime().strptime('16:00','%H:%M')
+	
+	#before open time
+	if time<=time_open:
+		score[date]+=float(r.score)
+	#after close time
+	elif time>time_close:
+		date=date.split('-')
+		day=date[2]
+		day=int(day)+1
+		new_date=date[0]+'-'+date[1]+'-'+str(day)
+		score[new_date]+=float(r.score)
+	elif time in range(time_open,time_close):
+		pass
 
 for k,v in od.items():
 	score[k]=score[k]/v
@@ -32,8 +51,8 @@ for k,v in od.items():
 
 df=pd.DataFrame(score,index=['score'])
 df=df.transpose()
-df.to_csv('REL_score.csv',sep=',',encoding='utf-8')
-df.to_json('REL_score.json')
+df.to_csv('REL_score_open.csv',sep=',',encoding='utf-8')
+df.to_json('REL_score_open.json')
 
 
 
