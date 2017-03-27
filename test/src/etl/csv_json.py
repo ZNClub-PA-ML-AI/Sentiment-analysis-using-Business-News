76186ca8df8d099c1ed009e464405934ea7c87bc
root <root@beproject.c.luminous-girder-160413.internal>
2017-03-27 11:49:39 +0000

    optimized csv_json to accept quandl and other csv types too

--- test/src/etl/csv_json.py
+++ test/src/etl/csv_json.py
@@ -6,11 +6,13 @@ import pandas as pd
 
 #print(sys.argv)
 
-filename=str(sys.argv[1])
+filename,type=sys.argv
 df=pd.read_csv(filename)
-#df = df.set_index(['date'])
-df = df.sort_values(['Date'],ascending=[1])
+
+if type=='q':
+	df = df.sort_values(['Date'],ascending=[1])
 print(df.head(1))
+
 #print(filename[:-3])
 df.to_json('../../../data/json/'+filename[:-3]+'json')
 
