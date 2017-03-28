2ce76b05c52912b4c4a451bd54923aacb09a2c43
root <root@beproject.c.luminous-girder-160413.internal>
2017-03-28 11:57:02 +0000

    testing merge 1

--- test/src/etl/merge_quandl.py
+++ test/src/etl/merge_quandl.py
@@ -9,5 +9,16 @@ filenames=[sys.argv[1],sys.argv[2]]
 
 #sentiment csv
 df1=pd.read_csv(filenames[0])
+df1=df1.set_index('date')
+#print(df1.head(1))
 
+#quandl csv
+df2=pd.read_csv(filenames[1])
+df2=df2.set_index('Date')
+#print(df2.head(1))
+
+df3=pd.concat([df1,df2],axis='1',join='inner')
+#print(df3.head(1),df3.columns,df3.describe())
+
+print(df1.shape)
 
