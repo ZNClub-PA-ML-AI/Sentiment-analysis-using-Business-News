d43398113e3d4c3b7dac13b56aa9595873388df3
root <root@beproject.c.luminous-girder-160413.internal>
2017-03-28 05:30:57 +0000

    starting to merge quandl and sentiment csv

---
+++ test/src/etl/merge_quandl.py
@@ -0,0 +1,13 @@
+
+
+
+
+import pandas as pd
+import sys
+
+filenames=[sys.argv[1],sys.argv[2]]
+
+#sentiment csv
+df1=pd.read_csv(filenames[0])
+
+
--- test/src/etl/test.py
+++ test/src/etl/test.py
@@ -4,4 +4,11 @@ df=pd.read_csv('../../TCS_score_open.csv')
 
 print(df.head(1))
 
+df=pd.read_csv('../../REL_sentiment.csv')
+
+print(df.describe())
+
+df=df.set_index('date')
+
+print(df.head(2))
 
