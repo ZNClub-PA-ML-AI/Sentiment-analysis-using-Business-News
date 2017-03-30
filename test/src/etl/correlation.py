c1437a83360a27166b5954ba2ef7200bc8722c96
ZNevzz <dsouza.nevil45@gmail.com>
2017-03-30 10:46:39 +0000

    testing with linear regression

--- test/src/etl/correlation.py
+++ test/src/etl/correlation.py
@@ -7,9 +7,9 @@ import pandas as pd
 df=pd.read_csv('../../REL_qs.csv')
 
 #print(dir(df))
-result=df.corr()
+result=df.corr(method='pearson',min_periods=100)
 
 print(result)
-result.to_csv('../../REL_corr.csv',encoding='utf-8',sep=',')
+#result.to_csv('../../REL_corr.csv',encoding='utf-8',sep=',')
 
 
---
+++ test/src/etl/linear.py
@@ -0,0 +1,39 @@
+"""
+Created on Thu Mar 23 14:50:34 
+
+@author: ZNevzz
+"""
+
+#import quandl
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+
+#df = quandl.get("NSE/INFY", authtoken="-zLmnBx6NmesMSEA_2MU")
+df=pd.read_csv('../../REL_qs.csv')
+
+
+#print(df.head(1))
+#print(df.describe())
+#print(df.columns)
+#print(df.date)
+
+X=np.array(df[['open_score']])
+Y=np.array(df[['Open']])
+
+print(len(X),len(Y))
+x_train,x_test, y_train, y_test = cross_validation.train_test_split(X, Y, test_size=0.25)
+print(len(x_train),len(x_test),len(y_train),len(y_test))
+
+clf= LinearRegression(n_jobs=-1)
+clf.fit(x_train, y_train)
+confidence = clf.score(x_test, y_test)
+print('Linear Confidence Score: %f'%(confidence*100))
+
+clf= svm.SVC(kernel='linear',gamma=1)
+clf.fit(x_train, y_train.ravel())
+confidence = clf.score(x_test, y_test)
+print('SVR  Confidence Score: %f'%(confidence*100))
+
+
